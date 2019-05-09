0525749db041ee1b74416a6e8b5b7a97b35fc8b3
maggieezzat <maggieezzat9@gmail.com>
2019-05-09 11:33:43 +0200

    added training scripts

---
+++ pretraining_old_vocab_128.sh
@@ -0,0 +1,4 @@
+export BERT_BASE_DIR=gs://deep_speech_bucket/multi_cased_L-12_H-768_A-12
+
+python run_pretraining.py --input_file=/home/maggie/bert/tfrecord_lists/dewiki_old_vocab_128_train.csv --output_dir=gs://deep_speech_bucket/bert_checkpoints/old_vocab_128/ --do_train=True --do_eval=True --bert_config_file=$BERT_BASE_DIR/bert_config.json --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt --train_batch_size=128 --max_seq_length=128 --max_predictions_per_seq=20 --num_train_steps=4070000 --num_warmup_steps=407000 --learning_rate=2e-5 --use_tpu=True --tpu_name=$TPU_NAME --tpu_zone=us-central1-f --save_checkpoints_steps=50
+
