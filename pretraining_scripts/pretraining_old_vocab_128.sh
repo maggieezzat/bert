b5846bd4f5f8b7a00b89d074dffa22b23c4f5f92
maggieezzat <maggieezzat9@gmail.com>
2019-05-13 14:46:44 +0200

    modifying scripts

--- pretraining_scripts/pretraining_old_vocab_128.sh
+++ pretraining_scripts/pretraining_old_vocab_128.sh
@@ -8,7 +8,7 @@ python3 /home/maggie/bert/run_pretraining.py \
 --do_eval=True \
 --bert_config_file=$BERT_BASE_DIR/bert_config.json \
 --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
---train_batch_size=912 \
+--train_batch_size=896 \
 --max_seq_length=128 \
 --max_predictions_per_seq=20 \
 --num_train_steps=30000 \
