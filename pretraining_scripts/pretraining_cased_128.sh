e6cb8d863bb7e973c9470542cb38d0d2d1b275a3
maggieezzat <maggieezzat9@gmail.com>
2019-05-13 13:25:58 +0000

    adjusted learning rate for cased model

--- pretraining_scripts/pretraining_cased_128.sh
+++ pretraining_scripts/pretraining_cased_128.sh
@@ -12,8 +12,8 @@ python3 /home/maggie/bert/run_pretraining.py \
 --max_seq_length=128 \
 --max_predictions_per_seq=20 \
 --num_train_steps=30000 \
---num_warmup_steps=15000 \
---learning_rate=1e-4 \
+--num_warmup_steps=10000 \
+--learning_rate=2e-5 \
 --use_tpu=True \
 --tpu_name=$TPU_NAME \
 --tpu_zone=us-central1-f
