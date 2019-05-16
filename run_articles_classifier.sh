5096cbec18e990ac710bac858285b9fd80c3da8a
maggieezzat <maggieezzat9@gmail.com>
2019-05-16 15:29:06 +0200

    fixed a bug

--- run_articles_classifier.sh
+++ run_articles_classifier.sh
@@ -1,12 +1,12 @@
 python3 /home/maggie/bert/articles_classifier.py \
 --data_dir=gs://deep_speech_bucket/articles/ \
---bert_config_file=/home/maggie/bert/config/bert_config_uncased.json \
---vocab_file=/home/maggie/bert/vocab/vocab_uncased_1000.txt \
+--bert_config_file=/home/maggie/bert/config/bert_config_cased.json \
+--vocab_file=/home/maggie/bert/vocab/vocab_cased_1000.txt \
 --output_dir=gs://deep_speech_bucket/finetuning_articles/ \
---do_lower_case=True \
---do_train=False \
---do_eval=False \
+--do_lower_case=False \
+--do_train=True \
+--do_eval=True \
 --do_predict=True \
 --use_tpu=True \
 --tpu_name=$TPU_NAME \
---init_checkpoint=gs://deep_speech_bucket/my_uncased_model
+--init_checkpoint=gs://deep_speech_bucket/my_cased_model
