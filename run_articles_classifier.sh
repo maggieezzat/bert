ff35c8aaa31a8f2c0504899e2d695001c9da1386
maggieezzat <maggieezzat9@gmail.com>
2019-05-18 12:09:29 +0200

    articles script

--- run_articles_classifier.sh
+++ run_articles_classifier.sh
@@ -20,37 +20,37 @@
 #--data_dir=gs://deep_speech_bucket/articles/ \
 #--bert_config_file=$BERT_BASE_DIR/bert_config.json \
 #--vocab_file=$BERT_BASE_DIR/vocab.txt \
-#--output_dir=gs://deep_speech_bucket/finetuning_articles_original_model/ \
+#--output_dir=gs://deep_speech_bucket/articles_original_model_complete/ \
 #--do_lower_case=False \
 #--do_train=True \
 #--do_eval=True \
 #--do_predict=True \
 #--use_tpu=True \
 #--tpu_name=$TPU_NAME \
-#--init_checkpoint=gs://deep_speech_bucket/bert_checkpoints/original_model
-
-#python3 /home/maggie/bert/articles_classifier.py \
-#--data_dir=gs://deep_speech_bucket/articles/ \
-#--bert_config_file=/home/maggie/bert/config/bert_config_cased.json \
-#--vocab_file=/home/maggie/bert/vocab/vocab_cased_1000.txt \
-#--output_dir=gs://deep_speech_bucket/finetuning_articles_cased/ \
-#--do_lower_case=False \
-#--do_train=True \
-#--do_eval=True \
-#--do_predict=True \
-#--use_tpu=True \
-#--tpu_name=$TPU_NAME \
-#--init_checkpoint=gs://deep_speech_bucket/complete_cased_model
+#--init_checkpoint=gs://deep_speech_bucket/complete_original_model
 
 python3 /home/maggie/bert/articles_classifier.py \
 --data_dir=gs://deep_speech_bucket/articles/ \
---bert_config_file=/home/maggie/bert/config/bert_config_uncased.json \
---vocab_file=/home/maggie/bert/vocab/vocab_uncased_1000.txt \
---output_dir=gs://deep_speech_bucket/articles_uncased_complete/ \
+--bert_config_file=/home/maggie/bert/config/bert_config_cased.json \
+--vocab_file=/home/maggie/bert/vocab/vocab_cased_1000.txt \
+--output_dir=gs://deep_speech_bucket/articles_cased_complete/ \
 --do_lower_case=False \
 --do_train=True \
 --do_eval=True \
 --do_predict=True \
 --use_tpu=True \
 --tpu_name=$TPU_NAME \
---init_checkpoint=gs://deep_speech_bucket/complete_uncased_model
+--init_checkpoint=gs://deep_speech_bucket/complete_cased_model
+
+#python3 /home/maggie/bert/articles_classifier.py \
+#--data_dir=gs://deep_speech_bucket/articles/ \
+#--bert_config_file=/home/maggie/bert/config/bert_config_uncased.json \
+#--vocab_file=/home/maggie/bert/vocab/vocab_uncased_1000.txt \
+#--output_dir=gs://deep_speech_bucket/articles_uncased_complete/ \
+#--do_lower_case=False \
+#--do_train=True \
+#--do_eval=True \
+#--do_predict=True \
+#--use_tpu=True \
+#--tpu_name=$TPU_NAME \
+#--init_checkpoint=gs://deep_speech_bucket/complete_uncased_model
