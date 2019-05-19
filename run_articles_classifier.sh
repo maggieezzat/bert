92da64ffcc80c8c7ab8eb7b044992b5398be2d81
maggieezzat <maggieezzat9@gmail.com>
2019-05-19 10:49:46 +0200

    .

--- run_articles_classifier.sh
+++ run_articles_classifier.sh
@@ -50,15 +50,15 @@
 
 
 python3 /home/maggie/bert/articles_classifier.py \
---data_dir=gs://deep_speech_bucket/articles/ \
+--data_dir=gs://deep_speech_bucket/articles_2/ \
 --bert_config_file=/home/maggie/bert/config/bert_config_uncased.json \
 --vocab_file=/home/maggie/bert/vocab/vocab_uncased_1000.txt \
---output_dir=gs://deep_speech_bucket/articles_uncased_trial/ \
+--output_dir=gs://deep_speech_bucket/articles_uncased_no_inference/ \
 --do_lower_case=False \
 --do_train=True \
 --do_eval=True \
 --do_predict=True \
 --use_tpu=True \
 --tpu_name=$TPU_NAME \
---init_checkpoint=gs://deep_speech_bucket/bert_checkpoints/uncased_model_2
+--init_checkpoint=gs://deep_speech_bucket/bert_checkpoints/my_uncased_model
 
